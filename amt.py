4e9b146fe9b96e4e8d2e34ddda61dea5c3385ef4
Jon Kolb <kolbyjack@gmail.com>
2020-02-12 21:41:07 -0500

    Refactor as class

--- amt.py
+++ amt.py
@@ -25,139 +25,146 @@ tmdb.API_KEY = "b888b64c9155c26ade5659ea4dd60e64"
 
 BASE_PATH = "/srv/media/movies"
 
-def readNfo(title):
-    md = {}
+class Video(object):
+    def __init__(self, filepath):
+        self._dir, self._filename = os.path.split(filepath)
+        self._title, self._ext = os.path.splitext(self._filename)
+        self._md = {}
 
-    nfo_filename = "{}/{}.nfo".format(BASE_PATH, title)
-    if not os.path.isfile(nfo_filename):
-        return md
+    def _build_filepath(self, ext):
+        filename = "{}.{}".format(self._title, ext)
+        return os.path.join(self._dir, filename)
 
-    try:
+    def _read_nfo(self):
+        nfo_filename = self._build_filepath("nfo")
+        if not os.path.isfile(nfo_filename):
+            return
+
         tree = ET.parse(nfo_filename)
         root = tree.getroot()
         for child in root:
             if child.tag in ("id", "title", "plot", "rating"):
-                md[child.tag] = child.text
+                self._md[child.tag] = child.text
             elif child.tag == "capturedate":
-                md["year"] = child.text
+                self._md["year"] = child.text
             elif child.tag == "genre":
-                md.setdefault("genres", []).append(child.text)
-    except:
-        pass
+                self._md.setdefault("genres", []).append(child.text)
 
-    return md
+    def _write_nfo(self):
+        nfo_filename = self._build_filepath("nfo")
 
-def writeNfo(title, md):
-    nfo_filename = "{}/{}.nfo".format(BASE_PATH, title)
+        movie = ET.Element("movie")
+        ET.SubElement(movie, "id").text = self._md["id"]
+        #ET.SubElement(movie, "rating").text = str(self._md["rating"])
+        ET.SubElement(movie, "title").text = self._md["title"]
+        ET.SubElement(movie, "plot").text = self._md["plot"]
+        ET.SubElement(movie, "capturedate").text = str(self._md["year"])
+        for genre in self._md["genres"]:
+            ET.SubElement(movie, "genre").text = genre
 
-    movie = ET.Element("movie")
-    ET.SubElement(movie, "id").text = md["id"]
-    ET.SubElement(movie, "rating").text = str(md["rating"])
-    ET.SubElement(movie, "title").text = md["title"]
-    ET.SubElement(movie, "plot").text = md["plot"]
-    ET.SubElement(movie, "capturedate").text = str(md["year"])
-    for genre in md["genres"]:
-        ET.SubElement(movie, "genre").text = genre
+        with open(nfo_filename, "wb") as fp:
+            fp.write(ET.tostring(movie))
 
-    with open(nfo_filename, "wb") as fp:
-        fp.write(ET.tostring(movie))
+    def _fetch_metadata(self):
+        searchtitle = self._title
+        if searchtitle.endswith(", The"):
+            searchtitle = "The {}".format(searchtitle[:-5])
+        elif searchtitle.endswith(", A"):
+            searchtitle = "A {}".format(searchtitle[:-3])
 
-def fetchMetadata(title):
-    metadata = readNfo(title)
-    if "title" in metadata:
-        return metadata
-
-    print("Processing %s" % title)
-
-    searchtitle = title
-    if searchtitle.endswith(", The"):
-        searchtitle = "The {}".format(searchtitle[:-5])
-    elif searchtitle.endswith(", A"):
-        searchtitle = "A {}".format(searchtitle[:-3])
-
-    imdb = Imdb()
-    if "id" not in metadata:
-        print("Searching IMDB")
-        results = imdb.search_for_title(searchtitle)
-        movie_results = [r for r in results if r["type"] == "feature" and r["year"] is not None]
-
-        while len(movie_results) == 0:
-            searchtitle = input("No results for \"%s\" Enter alternate/correct movie title >> " % searchtitle)
-
+        imdb = Imdb()
+        if "id" not in self._md:
+            print("  * Searching IMDB")
             results = imdb.search_for_title(searchtitle)
             movie_results = [r for r in results if r["type"] == "feature" and r["year"] is not None]
 
-        exact_matches = [r for r in movie_results if r["title"].lower() == searchtitle.lower()]
+            while len(movie_results) == 0:
+                searchtitle = input("No results for \"%s\" Enter alternate/correct movie title >> " % searchtitle)
 
-        if len(exact_matches) > 0:
-            movie_results = exact_matches
+                results = imdb.search_for_title(searchtitle)
+                movie_results = [r for r in results if r["type"] == "feature" and r["year"] is not None]
 
-        if len(movie_results) > 1:
-            choices=[("%s (%s)" % (r["title"], r["year"]), idx) for idx, r in enumerate(movie_results)]
-            choices.append(("Not found", -1))
-            answer = inquirer.prompt([
-                inquirer.List("index",
-                    message="Multiple results found:",
-                    choices=choices
-                )
-            ])
-            if answer["index"] == -1:
-                metadata["id"] = input("Enter IMDB id: ")
-                movie_results = []
-            else:
-                movie_results = [movie_results[answer["index"]]]
-        
-        if len(movie_results) > 0:
-            mpr = movie_results[0]
-            metadata["id"] = mpr["imdb_id"]
-            print("Fetching data for {} ({})".format(mpr["title"], mpr["year"]))
-    else:
-        print("Fetching data for %s" % metadata["id"])
+            exact_matches = [r for r in movie_results if r["title"].lower() == searchtitle.lower()]
 
-    imdb_movie = imdb.get_title(metadata["id"])
+            if len(exact_matches) > 0:
+                movie_results = exact_matches
 
-    metadata["title"] = imdb_movie["base"]["title"]
-    metadata["year"] = imdb_movie["base"]["year"]
-    metadata["rating"] = imdb_movie["ratings"]["rating"]
+            if len(movie_results) > 1:
+                choices=[("%s (%s)" % (r["title"], r["year"]), idx) for idx, r in enumerate(movie_results)]
+                choices.append(("Not found", -1))
+                answer = inquirer.prompt([
+                    inquirer.List("index",
+                        message="Multiple results found:",
+                        choices=choices
+                    )
+                ])
+                if answer["index"] == -1:
+                    self._md["id"] = input("Enter IMDB id: ")
+                    movie_results = []
+                else:
+                    movie_results = [movie_results[answer["index"]]]
 
-    if "outline" in imdb_movie["plot"]:
-        metadata["plot"] = imdb_movie["plot"]["outline"]["text"]
-    else:
-        metadata["plot"] = imdb_movie["plot"]["summaries"][0]["text"]
+            if len(movie_results) > 0:
+                mpr = movie_results[0]
+                self._md["id"] = mpr["imdb_id"]
+                print("  * Fetching data for {} ({})".format(mpr["title"], mpr["year"]))
+        else:
+            print("  * Fetching data for %s" % self._md["id"])
 
-    metadata["genres"] = imdb.get_title_genres(metadata["id"])["genres"]
+        imdb_movie = imdb.get_title(self._md["id"])
 
-    writeNfo(title, metadata)
+        self._md["title"] = imdb_movie["base"]["title"]
+        self._md["year"] = imdb_movie["base"]["year"]
+        #self._md["rating"] = imdb_movie["ratings"]["rating"]
 
-    return metadata
+        if "outline" in imdb_movie["plot"]:
+            self._md["plot"] = imdb_movie["plot"]["outline"]["text"]
+        else:
+            self._md["plot"] = imdb_movie["plot"]["summaries"][0]["text"]
 
-def fetchPoster(title, md):
-    poster_filename = "{}/{}.jpg".format(BASE_PATH, title)
-    if os.path.isfile(poster_filename):
-        return
+        self._md["genres"] = imdb.get_title_genres(self._md["id"])["genres"]
 
-    print("Fetching the movie poster...")
-    tmdb_find = tmdb.Find(md["id"])
-    tmdb_find.info(external_source="imdb_id")
+        self._write_nfo()
 
-    poster_url = r"https://image.tmdb.org/t/p/w780{}".format(tmdb_find.movie_results[0]["poster_path"])
+    def _fetch_poster(self):
+        poster_filename = self._build_filepath("jpg")
+        if os.path.isfile(poster_filename):
+            return
 
-    uo = urllib.request.urlopen(poster_url)
-    with open(poster_filename, "wb") as poster_file:
-        poster_file.write(uo.read())
-        poster_file.close()
+        print("  * Fetching the movie poster...")
+        tmdb_find = tmdb.Find(self._md["id"])
+        tmdb_find.info(external_source="imdb_id")
 
-def processMovies():
-    allMovies = sorted([fn for fn in os.listdir(BASE_PATH) if fn.endswith(".mp4")])
+        poster_url = r"https://image.tmdb.org/t/p/w780{}".format(tmdb_find.movie_results[0]["poster_path"])
 
-    for filename in allMovies:
+        uo = urllib.request.urlopen(poster_url)
+        with open(poster_filename, "wb") as poster_file:
+            poster_file.write(uo.read())
+            poster_file.close()
+
+    def _dirty(self):
+        if not os.path.exists(self._build_filepath("jpg")):
+            return True
+
+        self._read_nfo()
+        if "title" not in self._md:
+            return True
+
+        return False
+
+    def process(self):
         try:
-            title = filename[:-4]
-            md = fetchMetadata(title)
-            if md is not None:
-                writeNfo(title, md)
-                fetchPoster(title, md)
-        except Exception as e:
-            print("\nERROR processing %s: %s\n" % (filename, e))
+            if not self._dirty():
+                return
 
-processMovies()
+            print("Processing {}".format(self._title))
+            self._read_nfo()
+            self._fetch_metadata()
+            self._write_nfo()
+            self._fetch_poster()
+        except Exception as e:
+            print("  *** ERROR processing {}: {}".format(filename, e))
+
+allMovies = sorted([os.path.join(BASE_PATH, fn) for fn in os.listdir(BASE_PATH) if fn.endswith(".mp4")])
+for filename in allMovies:
+    Video(filename).process()
